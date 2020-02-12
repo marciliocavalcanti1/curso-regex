c8fee9b77744d54e038a302a353e3697a2ecd532
Marcílio Cavalcanti <m.matos.cavalcanti@avanade.com>
2020-02-12 10:50:16 -0300

    refatorando o bloco try/catch

--- src/caracteres/ProblemaPonto.java
+++ src/caracteres/ProblemaPonto.java
@@ -5,23 +5,23 @@ import java.util.regex.*;
 public class ProblemaPonto {
 	
 	private static final String texto = "Bom\ndia";
-	private static final String regex = "(?i)(?s)."; // flag (?s) resolve o problema do ponto. conceito chamado dotall
-//	private static final String regex = "(?i)...."; // o ponto não engloba o \n
+//	private static final String regex = "(?i)(?s)."; // flag (?s) resolve o problema do ponto. conceito chamado dotall
+	private static final String regex = "(?i)...."; // o ponto não engloba o \n
 	private static String error = "";
 	
 	public static void main(String[] args) {
 		
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(texto);
-		
 		try {
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(texto);
+			
 			while(matcher.find()) {
 				System.out.printf("%s %s%n", matcher.start(), matcher.group());
 			}
 		} catch (NullPointerException e) {
 			// TODO: handle exception
 			error = e.getMessage();
+			System.err.println(error);
 		}
 	}
-
 }
